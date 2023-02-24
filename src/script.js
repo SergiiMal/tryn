fe6d12098fb17aa5003b1224ff5c40aef6d1838e
SergiiMal <ser4ester@gmail.com>
2023-02-24 14:28:13 +0200

    +

--- src/script.js
+++ src/script.js
@@ -1,75 +1,206 @@
-// let a = 5;
-// let b = a;
-// console.log(a, b)
-// ---------------------------------------------
+// const propName = "name";
+// const user = {
+// 	age: 25,
+// 	[propName]: "Genri",
+// };
+// console.log(Object.values(user));
+// console.log(Object.keys(user));
+// console.log(user);
+// console.log(user.name);
+// -----------------------------------------------------
+
+// const aprtment = {
+// 	descr: "Special",
+// 	rating: 4,
+// 	price: 2015,
+// };
+// const values = [];
+// const keys = Object.keys(aprtment);
+// for (const key of keys) {
+
+// 	console.log(key)
+
+// 	values.push(aprtment[key]);
+// }
+// console.log(values);
+// -----------------------------------------------------
+
+// const user = {
+// 	name: "Sergiy",
+// 	age: 30,
+// };
+// console.table(user);
+// console.dir(user)
+// -----------------------------------------------------
+
+
+// Метод БАБЕЛСОРТ==========
+
+
+// const langs = ["python", "javascript", "c++", "php", "ruby"];
+// const langeSorted = langs.sort();
+
+// console.log("file: script.js:38  langeSorted:", langeSorted)
+
+// for (const i in langs) {
+// 	let wasSwap = false;
+// 	console.log("file: script.js:47  wasSwap:", wasSwap)
+// 	for (let j = 0; j < langs.length - 1 - i; j++){
+// 		if (langs[j].charCodeAt(0) > langs[j + 1].charCodeAt(0)) {
+// 			const swap = langs[j];
+// 			langs[j] = langs[j + 1];
+// 			langs[j + 1] = swap;
+// 			wasSwap = true;
+// 		}
+// 		if (!wasSwap) break;
+// 	}
+// }
+
+// console.log(langs)
+// ----------------------------------------------------
+
+// const arr = ['first', 'second'];
+// console.log(arr);
+
+// const swap = arr[0];//'first'
+// arr[0] = arr[1];//['second', 'second']
+// arr[1] = swap;//['second', 'first']
+// console.log(arr)
+// ----------------------------------------------------
+
+// const aprtment = {
+// 	descr: "Special",
+// 	rating: 4,
+// 	price: 2015,
+// };
+// const keys = [];
+// const values = [];
+// for (const key in aprtment) {
+// 	keys.push(key);
+// 	values.push(aprtment[key]);
+// }
+// 	console.log(aprtment)
+
+
+// const user = {
+// 	name: "Mango",
+// 	age: 35,
+// 	hobby: "html",
+// 	premium: true,
+// };
+// user.mood = "happi";
+// user.hobby = "skydiving";
+// user.premium = false;
+// const userKey = Object.keys(user)
+
+// for (const key of userKey) {
+// 	console.log(`${key}:${user[key]}`)
+// }
+// ----------------------------------------------------
+
+// const salaries = {
+// 	Jon: 100,
+// 	Ann: 160,
+// 	Pete: 130,
+// };
+// const salarArr = Object.values(salaries);
+// let sum = 0;
+// for (const salary of salarArr) {
+// 	sum += salary;
 	
-// const arr = [1, 2, 3];
-// const arr2 = [5, 6, 7];
-// const  arr3 = arr - arr2
-// // console.log(arr, arr2)
-// console.log(arr3)
-	// --------------------------------------------- 
-
-// function add(a, b) {
-// 	console.log(a + b);
 // }
-// const sum = add(5, 6);
 // console.log(sum);
+// ----------------------------------------------------
 
-// const sum2 = add(sum, 10);
-// console.log(sum2)
-	// --------------------------------------------- 
+// const stones = [
+// 	{ name: "Smaragd", price: 1300, quantity: 4 },
+// 	{ name: "Diamand", price: 2700, quantity: 3 },
+// 	{ name: "Sapfir", price: 400, quantity: 7 },
+// 	{ name: "Schebin", price: 200, quantity: 2 },
+// ];
+
+// console.log(calcTotalPrice(stones, 'Schebin'))
+
+// function calcTotalPrice(stounes, stoneName) {
+	
+// 	for (const stone of stounes) {
+// 		if (stone.name === stoneName) {
 			
-// function addPosition(a, b) {
-// 			if (a >= 0 && b >= 0) {
-// 				return a + b;
-// 			} else {
-// 				return 0;
-// 	 }
-
-// 	// return a + b;
+// 		return 	stone.price*stone.quantity
+// 			}
 // 	}
-
-// const sum = addPosition(-5, 6);
-// const sum2 = addPosition(sum, 10);
-// console.log(sum2);
-// ---------------------------------------------
-
-// const bmi = calcBMI('88,3', '1,75');
-// console.log(bmi);
-
-// function calcBMI(weight, height) {
-// 	weight = replaceCommaToDot(weight);
-// 	height = replaceCommaToDot(height);
-// 	return getRoundedNumber(weight / height ** 2); // 88.3 / 1.75 * 1.75(в квадраті)	// 28.8
+	
 // }
+// ----------------------------------------------------
 
-// // Перетворення коми а крапку
+//-------------------------- Internet BANK---------------------
+ 
+// const transactionTypes = {
+// 	DEPOSIT: 'deposit',
+// 	WITHDRAW: 'withdraw',
+// };
 
-// function replaceCommaToDot(string) {
-// 	return string.replace(',', '.');
-// }
+// const account = {
+// 	balance : 0,
+// 	transactions: [],
+// 	createTransaction(amount, type) {
+// 		return {
+// 			id: this.transactions.length,
+// 			amount,
+// 			type,
+// 		}
+// 	},
+// 	deposit(amount) {
+// 		this.balance += amount;
+// 		const transaction = this.createTransaction(amount, transactionTypes.DEPOSIT);
+// 		this.transactions.push(transaction);
+// 	},
+// 	withdraw(amount) {
+// 		if (amount > this.balance) {
+// 			console.log("NO kech")
+// 		}
+// 		this.balance -= amount;
+// 		const transaction = this.createTransaction(amount, transactionTypes.WITHDRAW);
+// 		this.transactions.push(transaction);
+// 	},
+// 	getBalance() {
+// 		return this.balance;
+// 	},
+// 	getTransactionDetailis(id) {
+// 		for (const transaction of this.transactions) {
+// 			if (id === transaction.id)
+// 				return transaction;
+// 			}
+// 	},
+// 	getTransactionTotal(type) {
+// 		let total = 0;
+// 		for (const transaction of this.transactions) {
+// 			if (type === transaction.type) total += transaction.amount;
+			
+// 		}
+// 		return total;
+// 	},
 
-// // Округлення цифри до першого знаку після крапки
+// };
+// console.log(account.getBalance());
 
-// function getRoundedNumber(number) {
-// 	return Number(number.toFixed(1));
-// }
-// ---------------------------------------------
+// account.deposit(500);
+// console.log(account.getBalance());
 
-// Найбільше число в масиві
+// account.withdraw(200);
+// account.withdraw(500);
 
-// function findLargestNumber(numbers) {
-// 	let max = numbers[0];
-// 	for (const number of numbers) {
-// 		if (number > max) max = number;
-// 	}
-// 	return max
-// };	
+// account.deposit(1000);
 
-// console.log(findLargestNumber([2, 17 , 94 , 1, 23, 37]))
-// console.log(findLargestNumber([49, 4, 7, 83, 12]))
+// account.withdraw(100);
+// account.withdraw(200);
+// account.withdraw(300);
 
-// ---------------------------------------------
+// console.log(account.getBalance());
 
-cdsdsc
+// console.log(account.getTransactionDetailis(1));
+
+// console.log(account.getTransactionTotal(transactionTypes.WITHDRAW));
+// console.log(account.getTransactionTotal(transactionTypes.DEPOSIT));
+
+// console.log(account);
